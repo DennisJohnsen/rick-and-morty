695e0e04db90250532a211d4004185c079dcaea5
DennisJohnsen <dennis.k.johnsen@gmail.com>
2024-05-14 16:23:30 +0200

    Configured next for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/rick-and-morty",
+};
 
 export default nextConfig;
