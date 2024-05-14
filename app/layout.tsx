6b606df91aa792926eba58392466110dd0d49cf3
DennisJohnsen <dennis.k.johnsen@gmail.com>
2024-05-14 16:16:38 +0200

    Init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rick and Morty",
+  description: "A detailed description of all Rick and Morty characters.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ components/charactersList.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+export const CharactersList = () => {
+    const [characters, setCharacters] = useState<{ id: number; name: string }[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://rickandmortyapi.com/api/character"
+        );
+        const data = await response.json();
+        setCharacters(data.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Characters</h1>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.id}>{character.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
